ba8bb290887ab2ca9c0e83420ccb9d6961934a1a
Washington Freitas <washington@viewup.com.br>
2019-12-02 22:26:27 -0300

    Adicionado a persistencia de conexao

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { MongoClient, FilterQuery, Db, Collection, MongoClientOptions } from "mongodb";
+import { MongoClient, Db, Collection, MongoClientOptions } from "mongodb";
 
 export interface ActionCreateCallbackParams {
     db: Db;
@@ -12,25 +12,82 @@ export interface ActionCreatorParams {
     clientOptions?: MongoClientOptions;
 
 }
-export function closeClient(client: MongoClient) {
-    client.close();
-}
-const connections: { [dbName: string]: MongoClient } = {};
+const instances: { [uri: string]: MongoClient } = {};
 
-export function getClient(dbName: string, uri: string, options?: MongoClientOptions): MongoClient {
-    if (connections[dbName]) {
-        return connections[dbName];
+
+export function addConnection(uri: string, dbName: string, clientOptions?: MongoClientOptions): Promise<MongoClient> {
+    return new Promise((resolve) => {
+        const client = new MongoClient(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            connectTimeoutMS: 2 * 60 * 60 * 1000,
+            socketTimeoutMS: 2 * 60 * 60 * 1000,
+            ...clientOptions
+        });
+        client.connect(async (err, _client) => {
+            if (err) {
+                console.log("Connection error");
+                throw err;
+            }
+
+            console.log("Connected successfully to server " + uri);
+
+            instances[dbName] = client;
+            resolve(client);
+        });
+    })
+}
+// const dbInstances = {};
+export async function connect<TResult>(dbName: string, collectionName: string, cb: (params: ActionCreateCallbackParams) => Promise<TResult>): Promise<TResult> {
+
+    const _client = instances[dbName];
+    if (!_client) {
+        throw new Error('Has no instance for ' + dbName)
     }
-    connections[dbName] = new MongoClient(uri, { useNewUrlParser: true, ...options });
-    return connections[dbName];
+    const db = _client.db(dbName);
+    return await cb({ db, collection: db.collection(collectionName) });
+}
+async function getClient<TResult = any>(dbName: string, collectionName: string, cb: (params: ActionCreateCallbackParams) => Promise<TResult>): Promise<TResult> {
+
+
+    try {
+        return await connect<TResult>(dbName, collectionName, cb);
+    } catch (e) {
+        console.log("CLIENT NOT CONNECTED")
+        throw e;
+    }
+
+
+
+}
+export interface ActionCreateCallbackParams {
+    db: Db;
+    collection: Collection;
+
 }
 
-export function mongoFnCreator({ dbName, uri, clientOptions }: ActionCreatorParams) {
+export function closeClient(dbName: string) {
+
+    if (!instances[dbName]) {
+        return null;
+    }
+
+    instances[dbName].close();
+
+}
+// export async function actionCreator<TResult = any>({ collectionName, dbName }: ActionCreatorParams, cb: (params: ActionCreateCallbackParams) => Promise<TResult>): Promise<TResult> {
+//     return await getClient<TResult>(dbName || 'pedidosOnline', collectionName, cb);
+// }
+
+export function mongoFnCreator({ dbName, uri }: ActionCreatorParams) {
+    if (uri) {
+        console.warn("URI IS DEPRECATED, USE addConnection");
+    }
     return async <TResult>(collectionName: string, executor: (params: ActionCreateCallbackParams) => Promise<TResult>) => {
-        const client = getClient(dbName, uri, clientOptions);
-        await client.connect();
-        const db = client.db(dbName);
-        return await executor({ db, collection: db.collection(collectionName) });
+        return await getClient<TResult>(dbName || 'test', collectionName, executor);
+
+        // const db = client.db(dbName);
+        // return await executor({ db, collection: db.collection(collectionName) });
         // closeClient(client);
 
     }
--- src/test.ts
+++ src/test.ts
@@ -1,9 +1,10 @@
-import { mongoFnCreator } from './index'
+import { ObjectId } from 'bson';
+import { addConnection, closeClient, mongoFnCreator } from './index';
 
 // todo database factor
 
 const todoTransactions = mongoFnCreator({
-    uri: 'mongodb://localhost:27017',
+    uri: '',
     dbName: 'MyDatabaseName'
 })
 
@@ -15,10 +16,18 @@ interface TodoType {
 
 
 (async function listTodos() {
+    await addConnection('mongodb://localhost:27017', 'MyDatabaseName');
+    await todoTransactions<any>(
+        'Todos',
+        // do what you want to do
+        ({ collection }) => collection.findOneAndUpdate({ _id: new ObjectId() }, { $set: { complete: false, title: 'teste' } }, { upsert: true })
+    );
     const todos = await todoTransactions<TodoType[]>(
         'Todos',
         // do what you want to do
         ({ collection }) => collection.find({}).toArray()
     );
-    console.log(todos.map((todo) => `${todo.title}: ${todo.complete ? 'complete' : 'incomplete'}`))
+    console.log(todos.map((todo) => `${todo.title}: ${todo.complete ? 'complete' : 'incomplete'}`));
+    await closeClient('MyDatabaseName')
+    process.exit(0)
 })()
